e9a6de187a7b32a7c58a9f122ee6b2e4ad88a2dd
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2024-08-01 16:56:55 +0530

    day#4 update#2

---
+++ basic-js/calc_app/calc.js
@@ -0,0 +1,77 @@
+
+
+
+
+class Calculator{
+    constructor(presenter){
+        this.operators={};
+        this.addDefaultOperators();
+
+        this.presenter=presenter || console.log;
+    }
+
+    addDefaultOperators(){
+        this.addOperator((x,y)=>x+y, "plus");
+        this.addOperator((x,y)=>x-y, "minus");
+        this.addOperator((x,y)=>x*y, "multiply");
+        this.addOperator((x,y)=>x/y, "divide");
+
+    }
+
+
+    //socket for future extension.
+    addOperator(operator,...names){
+        if(operator.name)
+            this.operators[operator.name]=operator;
+        
+        for(let name of names){
+            this.operators[name]=operator;
+        }
+    }
+
+    setPresenter(presenter){
+        this.presenter=presenter;
+    }
+
+
+    execute(value1,operatorName, value2){
+        if(this.operators[operatorName]){
+            //Step #1 Find operator
+            var operator=this.operators[operatorName];
+
+            //Step #2 Do Calculation
+            var result=operator(value1,value2);
+
+            //Step #3 Format the output
+            var output=`${value1} ${operatorName} ${value2} = ${result}`;
+
+            //Step #4 Present the result
+            // hard coded dependency on console.
+            //console.log(output);
+
+            //dependency inversion... not knowing exact dependency.
+            //I need to present but not sure where/how.
+            this.presenter(output); 
+
+
+        }else{
+            //Present the Error Message
+            //console.log(`Invalid operator: ${operatorName}`);
+            this.presenter(`Invalid operator: ${operatorName}`); 
+        }
+    }
+}
+
+function testCalculator(){
+    var calc=new Calculator();
+    calc.execute(10,"plus",5);
+    calc.execute(10,"minus",5);
+
+    calc.setPresenter( output=> console.log(`*** ${output} ***`) );
+
+    calc.execute(10,"multiply",5);
+    calc.execute(10,"divide",4);
+    calc.execute(10,"mod",4);
+}
+
+testCalculator();
